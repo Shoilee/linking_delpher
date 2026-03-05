ac515c9d6989ffc0d7ca977b0bfb216488486798
Shoilee <sarahshoilee121@gmail.com>
2026-03-05 14:53:44 +0100

    Add OCR text to the document

--- src/create_article_docs.py
+++ src/create_article_docs.py
@@ -2,6 +2,7 @@
 import os
 from lxml import etree
 import xmltodict, json
+import requests
 
 def get_all_articles(root):
     articles_xml = root.findall('.//didl:Item[@ddd:article_id]', 
@@ -12,6 +13,18 @@ def get_all_articles(root):
                                 )  
     # print(f"Found {len(articles_xml)} articles in the XML.")
     return articles_xml
+
+def get_article_text(identifier):
+    URI = f"http://resolver.kb.nl/resolve?urn={identifier}:ocr"
+    print(f"Retrieving OCR text for identifier: {identifier}")
+    req = requests.get(URI)
+    if req.status_code == 200:
+        root = etree.fromstring(req.text.encode('utf-8'))
+        text_children = list(root)
+        clean_xml_string = (''.join([etree.tostring(child, pretty_print=True).decode('utf-8') for child in text_children]))
+        return clean_xml_string
+    else:
+        print(f"Failed to retrieve OCR text for identifier: {identifier}, status code: {req.status_code}")
 
 if __name__ == '__main__':
     OUTPUT_DIR = 'data/DST'
@@ -24,11 +37,14 @@ if __name__ == '__main__':
     for article in articles_xml:
         article_id = article.get('{http://www.kb.nl/namespaces/ddd}article_id')
         print(f"Processing article ID: {article_id}")
+
+        text = get_article_text(article_id)
         
         xml_bytes = etree.tostring(article)        
         data = xmltodict.parse(xml_bytes)["didl:Item"]      
-        
-        # TODO: Add article OCRED text to data dict here
+
+        # Add the retrieved OCR text to the data dictionary
+        data['ocr_text'] = text
 
         output_path = f'{OUTPUT_DIR}/{article_id.replace(":", "_")}.json'
         with open(output_path, 'w', encoding='utf-8') as json_file:
