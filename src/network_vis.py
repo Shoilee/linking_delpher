a8ad7ded8296f267a3e8078b33c2b35e015b8623
Shoilee <sarahshoilee121@gmail.com>
2026-07-24 09:55:45 +0200

    update file path

--- src/network_vis.py
+++ src/network_vis.py
@@ -6,7 +6,7 @@ from st_link_analysis import st_link_analysis, NodeStyle, EdgeStyle
 
 st.set_page_config(layout="wide")
 
-GRAPH_PATH = Path(__file__).with_name("graph_output.json")
+GRAPH_PATH = Path("DST/graph_output.json")
 
 
 def clean_edges(elements):
