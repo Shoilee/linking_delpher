79b70d6b35be622b90214f53560e92a2e220a55a
Shoilee <sarahshoilee121@gmail.com>
2026-07-28 15:03:25 +0200

    minor

--- multipage_app/home.py
+++ multipage_app/home.py
@@ -17,7 +17,7 @@ if "histEvent_startDate" not in st.session_state:
 if "histEvent_endDate" not in st.session_state:
     st.session_state["histEvent_endDate"] = None
 
-event_title = st.text_input("Enter your event title", st.session_state["histEvent_title"])
+event_title = st.text_input("Enter your historical event title", st.session_state["histEvent_title"])
 # use a real date default and pass session_state key via keyword to avoid positional-after-keyword error
 default_date = datetime.date(1700, 1, 1)
 event_startDate = st.date_input("Enter the (possible) event start date", value="1600-01-01", min_value="1600-01-01", max_value="1950-01-01")
